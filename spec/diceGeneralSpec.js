a956a8181bfc69bf7ef74f2eceeff97e75ac8224
Fernando Viton <fcode@viton.org>
2015-11-25 19:19:54 -0800

    Add chance version of rollPercentiles

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -8,4 +8,10 @@ describe("A suite", function() {
   it ("rollDice returns blah", function() {
     expect(Dice.rollDice2({d20: function() {return 5;}})).toBe(5);
   });
+
+  it("rollPercentiles returns digits each 0-9", function() {
+    expect(Dice.rollPercentiles({d10: function() {return 1;}})).toEqual([0,0]);
+    expect(Dice.rollPercentiles({d10: function() {return 10;}})).toEqual([9,9]);
+    expect(Dice.rollPercentiles({d10: function() {return 5;}})).toEqual([4,4]);
+  });
 });
