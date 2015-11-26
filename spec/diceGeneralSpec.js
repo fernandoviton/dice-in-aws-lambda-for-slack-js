8ad1278732dfe4291e21c7ebbfa7dc2abbaec3d7
Fernando Viton <fcode@viton.org>
2015-11-25 18:57:58 -0800

    add example of using chance during tests.  update package.json

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -4,4 +4,8 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(Dice.rollDice()).toBe(10);
   });
+
+  it ("rollDice returns blah", function() {
+    expect(Dice.rollDice2({d20: function() {return 5;}})).toBe(5);
+  });
 });
