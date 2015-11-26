9068c313928ede4852fbe889101cddf7b80eecd4
Fernando Viton <fcode@viton.org>
2015-11-25 22:24:58 -0800

    Refactor a few totalling methods into general

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -1,17 +1,21 @@
 var Dice = require('../lib/diceGeneral');
 
 describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(Dice.rollDice()).toBe(10);
-  });
-
-  it ("rollDice returns blah", function() {
-    expect(Dice.rollDice2({d20: function() {return 5;}})).toBe(5);
-  });
-
   it("rollPercentiles returns digits each 0-9", function() {
     expect(Dice.rollPercentiles({d10: function() {return 1;}})).toEqual([0,0]);
     expect(Dice.rollPercentiles({d10: function() {return 10;}})).toEqual([9,9]);
     expect(Dice.rollPercentiles({d10: function() {return 5;}})).toEqual([4,4]);
+  });
+
+  it("getting total of rollPercentiles", function() {
+    expect(Dice.totalOfPercentilesRoll([50, 6])).toEqual(56);
+    expect(Dice.totalOfPercentilesRoll([90, 9])).toEqual(99);
+    expect(Dice.totalOfPercentilesRoll([0, 0])).toEqual(100);
+  });
+
+  if("getting total of rolls", function() {
+    expect(Dice.totalOfRoll([])).toEqual(0);
+    expect(Dice.totalOfRoll([6])).toEqual(6);
+    expect(Dice.totalOfRoll([6, 7])).toEqual(13);
   });
 });
