bf4775fa501031ca0f338643c98a5e8a9d2679ca
Fernando Viton <fcode@viton.org>
2015-11-27 16:31:00 -0800

    create new rollDice method (not yet hooked up)

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -18,4 +18,31 @@ describe("Dice suite", function() {
     expect(Dice.totalOfRoll([6])).toEqual(6);
     expect(Dice.totalOfRoll([6, 7])).toEqual(13);
   });
+
+  it("rollDice called with invalid input", function() {
+    expect(function () {Dice.rollDice(null)}).toThrow("No Input Options");
+    expect(function () {Dice.rollDice(null, {blah: 5})}).toThrow("No Input Sides");
+  });
+
+  it("rollDice of (implicitly) single die", function() {
+    var chanceInteger = function(options, expectedMax, retVal)
+    {
+      if (options.min != 1) throw "min always expected as 1";
+      if (options.max != expectedMax) throw "max not as expected";
+      if (retVal > options.max) throw "retVal greater than max";
+      return retVal;
+    };
+    var chanceIntegerFor10 = function(options, retVal) {return chanceInteger(options, 10, retVal);};
+    var chanceIntegerFor3 = function(options, retVal) {return chanceInteger(options, 3, retVal);};
+    expect(Dice.rollDice({integer: function(options) {return chanceIntegerFor10(options, 1);}}, {sides:10})).toEqual([1]);
+    expect(Dice.rollDice({integer: function(options) {return chanceIntegerFor10(options, 5);}}, {sides:10})).toEqual([5]);
+    expect(Dice.rollDice({integer: function(options) {return chanceIntegerFor3(options, 1);}}, {sides:3})).toEqual([1]);
+    expect(Dice.rollDice({integer: function(options) {return chanceIntegerFor3(options, 3);}}, {sides:3})).toEqual([3]);
+  });
+
+  it("rollDice of multiple dice and explicit single die", function() {
+    expect(Dice.rollDice({integer: function(options) {return 3;}}, {sides:6, numOfDice:1})).toEqual([3]);
+    expect(Dice.rollDice({integer: function(options) {return 4;}}, {sides:6, numOfDice:3})).toEqual([4,4,4]);
+    expect(Dice.rollDice({integer: function(options) {return 5;}}, {sides:8, numOfDice:4})).toEqual([5,5,5,5]);
+  });
 });
