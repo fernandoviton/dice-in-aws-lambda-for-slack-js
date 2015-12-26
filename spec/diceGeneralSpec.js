bfea8a61aa121d2c69244f90f250d8f67b9c32e5
Fernando Viton <fcode@viton.org>
2015-12-26 11:38:28 -0300

    Changed percentile results to include 10s and 1s (not 2 1s).  This is what totalOfPercentilesRoll expects.

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -3,14 +3,15 @@ var Dice = require('../lib/diceGeneral');
 describe("Dice suite", function() {
   it("rollPercentiles returns digits each 0-9", function() {
     expect(Dice.rollPercentiles({d10: function() {return 1;}})).toEqual([0,0]);
-    expect(Dice.rollPercentiles({d10: function() {return 10;}})).toEqual([9,9]);
-    expect(Dice.rollPercentiles({d10: function() {return 5;}})).toEqual([4,4]);
+    expect(Dice.rollPercentiles({d10: function() {return 10;}})).toEqual([90,9]);
+    expect(Dice.rollPercentiles({d10: function() {return 5;}})).toEqual([40,4]);
   });
 
   it("getting total of rollPercentiles", function() {
     expect(Dice.totalOfPercentilesRoll([50, 6])).toEqual(56);
     expect(Dice.totalOfPercentilesRoll([90, 9])).toEqual(99);
     expect(Dice.totalOfPercentilesRoll([0, 0])).toEqual(100);
+    expect(Dice.totalOfPercentilesRoll(Dice.rollPercentiles({d10: function() {return 2;}}))).toEqual(11);
   });
 
   it("getting total of rolls", function() {
