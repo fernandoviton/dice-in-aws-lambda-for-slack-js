c287b5479431857b8baeb83523351fb8ab24df8e
Fernando Viton <fcode@viton.org>
2015-12-31 18:01:25 -0300

    Split tests into seperate describes

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -1,31 +1,33 @@
 var Dice = require('../lib/diceGeneral');
 
-describe("Dice suite", function() {
-  it("rollPercentiles returns digits each 0-9", function() {
+describe("rollPercentiles", function() {
+  it("returns digits each 0-9", function() {
     expect(Dice.rollPercentiles({d10: function() {return 1;}})).toEqual([0,0]);
     expect(Dice.rollPercentiles({d10: function() {return 10;}})).toEqual([90,9]);
     expect(Dice.rollPercentiles({d10: function() {return 5;}})).toEqual([40,4]);
   });
 
-  it("getting total of rollPercentiles", function() {
+  it("getting total", function() {
     expect(Dice.totalOfPercentilesRoll([50, 6])).toEqual(56);
     expect(Dice.totalOfPercentilesRoll([90, 9])).toEqual(99);
     expect(Dice.totalOfPercentilesRoll([0, 0])).toEqual(100);
     expect(Dice.totalOfPercentilesRoll(Dice.rollPercentiles({d10: function() {return 2;}}))).toEqual(11);
   });
+});
 
-  it("getting total of rolls", function() {
+describe("rollDice", function() {
+  it("getting total", function() {
     expect(function () {Dice.totalOfRoll([])}).toThrow("Empty Input");
     expect(Dice.totalOfRoll([6])).toEqual(6);
     expect(Dice.totalOfRoll([6, 7])).toEqual(13);
   });
 
-  it("rollDice called with invalid input", function() {
+  it("called with invalid input", function() {
     expect(function () {Dice.rollDice(null)}).toThrow("No Input Options");
     expect(function () {Dice.rollDice(null, {blah: 5})}).toThrow("No Input Sides");
   });
 
-  it("rollDice of (implicitly) single die", function() {
+  it("of (implicitly) single die", function() {
     var chanceInteger = function(options, expectedMax, retVal)
     {
       if (options.min != 1) throw "min always expected as 1";
@@ -41,13 +43,13 @@ describe("Dice suite", function() {
     expect(Dice.rollDice({integer: function(options) {return chanceIntegerFor3(options, 3);}}, {sides:3})).toEqual([3]);
   });
 
-  it("rollDice of multiple dice and explicit single die", function() {
+  it("of multiple dice and explicit single die", function() {
     expect(Dice.rollDice({integer: function(options) {return 3;}}, {sides:6, numOfDice:1})).toEqual([3]);
     expect(Dice.rollDice({integer: function(options) {return 4;}}, {sides:6, numOfDice:3})).toEqual([4,4,4]);
     expect(Dice.rollDice({integer: function(options) {return 5;}}, {sides:8, numOfDice:4})).toEqual([5,5,5,5]);
   });
 
-  it("rollDice of zero sides and/or numOfDice", function() {
+  it("of zero sides and/or numOfDice", function() {
     expect(Dice.rollDice({integer: function(options) {return 3;}}, {sides:0, numOfDice:1})).toEqual([]);
     expect(Dice.rollDice({integer: function(options) {return 3;}}, {sides:0, numOfDice:0})).toEqual([]);
     expect(Dice.rollDice({integer: function(options) {return 3;}}, {sides:1, numOfDice:0})).toEqual([]);
@@ -55,8 +57,6 @@ describe("Dice suite", function() {
 });
 
 describe("getRollDataFromParsedRoll", function() {
-  it("of empty rollData returns null", function() {
-  });
   it("of incomplete (invalid) rollData returns null", function() {
     expect(Dice.getRollDataFromParsedRoll([])).toBe(null);
     expect(Dice.getRollDataFromParsedRoll(["blah"])).toBe(null);
