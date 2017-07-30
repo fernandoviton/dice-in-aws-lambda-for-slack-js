7df3288aa21ec3015ae5e2460ee62f4b5b472df7
Fernando Viton <fcode@viton.org>
2017-07-30 00:19:39 -0400

    Add hook for testing

--- spec/diceGeneralSpec.js
+++ spec/diceGeneralSpec.js
@@ -1,11 +1,32 @@
 var All = require('../lib/all.js');
 
 describe("handler", function() {
+  it("with c", function() {
+    var responseData = null;
+    const roll = (min, max) => 4;
+    const doneFunc = (_, r) => { responseData = r; };
+    const errorFunc = (_, __) => {};
+    const result = All.handlerImpl(roll, 'myUser', 'c', doneFunc, errorFunc);
+    expect(result).toBe(true);
+    expect(responseData.text).toBe('myUser rolled percentiles and got *44* (40, 4)');
+  });
   it("with 1d4", function() {
-    var doneFunc = (_, __) => {};
-    var errorFunc = (_, __) => {};
-    const result = All.handlerImpl('user', '1d4', doneFunc, errorFunc);
-    expect(result).toBeGreaterThan(0);
-    expect(result).toBeLessThan(5);
+    var responseData = null;
+    const roll = (min, max) => 4;
+    const doneFunc = (_, r) => { responseData = r; };
+    const errorFunc = (_, __) => {};
+    const result = All.handlerImpl(roll, 'myUser', '1d4', doneFunc, errorFunc);
+    expect(result).toBe(true);
+    expect(responseData.text).toBe('myUser rolled 1d4 and got *4*');
+  });
+  it("with newCharacter", function() {
+    var responseData = null;
+    const roll = (min, max) => 4;
+    const doneFunc = (_, r) => { responseData = r; };
+    const errorFunc = (_, __) => {};
+    const result = All.handlerImpl(roll, 'myUser', '1d4', doneFunc, errorFunc);
+    expect(result).toBe(true);
+    expect(responseData.text).not.toBe(null);
   });
 });
+
